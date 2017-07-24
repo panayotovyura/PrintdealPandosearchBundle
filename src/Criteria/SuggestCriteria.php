cd994a483fab59245eb530b2a9b9d632a327c90c
oromanchenko <o.romanchenko@levi9.com>
2017-07-24 11:29:10 +0300

    infrastructure for suggestion done

---
+++ src/Criteria/SuggestCriteria.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Printdeal\PandosearchBundle\Criteria;
+
+use JMS\Serializer\Annotation as Serializer;
+
+final class SuggestCriteria
+{
+    /**
+     * @var string
+     * @Serializer\SerializedName("q")
+     * @Serializer\Type("string")
+     */
+    private $query;
+
+    /**
+     * @var bool
+     * @Serializer\Type("boolean")
+     */
+    private $track;
+
+    /**
+     * @param string $query
+     * @return SuggestCriteria
+     */
+    public function setQuery(string $query): SuggestCriteria
+    {
+        $this->query = $query;
+        return $this;
+    }
+
+    /**
+     * @param bool $track
+     * @return SuggestCriteria
+     */
+    public function setTrack(bool $track): SuggestCriteria
+    {
+        $this->track = $track;
+        return $this;
+    }
+}
--- src/Service/SearchService.php
+++ src/Service/SearchService.php
@@ -6,16 +6,30 @@ use GuzzleHttp\ClientInterface;
 use GuzzleHttp\Exception\TransferException;
 use JMS\Serializer\SerializerInterface;
 use Printdeal\PandosearchBundle\Builder\SearchCriteriaBuilder;
+use Printdeal\PandosearchBundle\Builder\SuggestCriteriaBuilder;
 use Printdeal\PandosearchBundle\Criteria\SearchCriteria;
+use Printdeal\PandosearchBundle\Criteria\SuggestCriteria;
 use Printdeal\PandosearchBundle\Exception\RequestException;
 use Printdeal\PandosearchBundle\Exception\SerializationException;
 
 class SearchService
 {
+    const SUGGEST_ENDPOINT = 'suggest';
+    const SEARCH_ENDPOINT = 'search';
+
+    const GET_METHOD = 'GET';
+
+    const JSON_ACCEPT = 'application/json';
+
     /**
      * @var SearchCriteriaBuilder
      */
-    private $criteriaBuilder;
+    private $searchCriteriaBuilder;
+
+    /**
+     * @var SuggestCriteriaBuilder
+     */
+    private $suggestCriteriaBuilder;
 
     /**
      * @var ClientInterface
@@ -30,16 +44,19 @@ class SearchService
     /**
      * SearchService constructor.
      * @param ClientInterface $httpClient
-     * @param SearchCriteriaBuilder $criteriaBuilder
+     * @param SearchCriteriaBuilder $searchCriteriaBuilder
+     * @param SuggestCriteriaBuilder $suggestCriteriaBuilder
      * @param SerializerInterface $serializer
      */
     public function __construct(
         ClientInterface $httpClient,
-        SearchCriteriaBuilder $criteriaBuilder,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        SuggestCriteriaBuilder $suggestCriteriaBuilder,
         SerializerInterface $serializer
     ) {
-        $this->criteriaBuilder = $criteriaBuilder;
         $this->httpClient = $httpClient;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->suggestCriteriaBuilder = $suggestCriteriaBuilder;
         $this->serializer = $serializer;
     }
 
@@ -53,12 +70,42 @@ class SearchService
     {
         try {
             $response = $this->httpClient->request(
-                'GET',
-                'search',
+                self::GET_METHOD,
+                self::SEARCH_ENDPOINT,
                 [
-                    'query' => $this->criteriaBuilder->build($criteria),
+                    'query' => $this->searchCriteriaBuilder->build($criteria),
                     'headers' => [
-                        'accept' => 'application/json',
+                        'accept' => self::JSON_ACCEPT,
+                    ]
+                ]
+            );
+        } catch (TransferException $exception) {
+            throw new RequestException($exception->getMessage(), $exception->getCode(), $exception);
+        }
+
+        try {
+            return $this->serializer->deserialize($response->getBody()->getContents(), 'array', 'json');
+        } catch (\Exception $exception) {
+            throw new SerializationException($exception->getMessage(), $exception->getCode(), $exception);
+        }
+    }
+
+    /**
+     * @param SuggestCriteria $criteria
+     * @return array
+     * @throws RequestException
+     * @throws SerializationException
+     */
+    public function suggest(SuggestCriteria $criteria): array
+    {
+        try {
+            $response = $this->httpClient->request(
+                self::GET_METHOD,
+                self::SUGGEST_ENDPOINT,
+                [
+                    'query' => $this->suggestCriteriaBuilder->build($criteria),
+                    'headers' => [
+                        'accept' => self::JSON_ACCEPT,
                     ]
                 ]
             );
