3de7abe53d0044d8b309090243fcb9d1977d144e
oromanchenko <o.romanchenko@levi9.com>
2017-07-25 16:52:06 +0300

    PSR fixed

--- src/Builder/BaseQueryBuilder.php
+++ src/Builder/BaseQueryBuilder.php
@@ -7,40 +7,40 @@ use Printdeal\PandosearchBundle\Criteria\SerializableInterface;
 
 abstract class BaseQueryBuilder
 {
-     /**
-      * @var ArrayTransformerInterface
-      */
-     protected $serializer;
-
-     /**
-      * @var array
-      */
-     private $overrides;
-
-     /**
-      * SearchCriteriaBuilder constructor.
-      * @param ArrayTransformerInterface $serializer
-      * @param array $queryOverrides
-      */
-     public function __construct(ArrayTransformerInterface $serializer, array $queryOverrides = [])
-     {
-         $this->serializer = $serializer;
-         $this->overrides = $queryOverrides;
-     }
+    /**
+    * @var ArrayTransformerInterface
+    */
+    protected $serializer;
 
     /**
-     * @param array $query
-     * @return array
-     */
-     protected function executeOverrides(array $query): array
-     {
-         return array_merge($this->overrides, $query);
-     }
+    * @var array
+    */
+    private $overrides;
 
     /**
-     * @param SerializableInterface $criteria
-     * @return array
-     */
+    * SearchCriteriaBuilder constructor.
+    * @param ArrayTransformerInterface $serializer
+    * @param array $queryOverrides
+    */
+    public function __construct(ArrayTransformerInterface $serializer, array $queryOverrides = [])
+    {
+        $this->serializer = $serializer;
+        $this->overrides = $queryOverrides;
+    }
+
+    /**
+    * @param array $query
+    * @return array
+    */
+    protected function executeOverrides(array $query): array
+    {
+        return array_merge($this->overrides, $query);
+    }
+
+    /**
+    * @param SerializableInterface $criteria
+    * @return array
+    */
     protected function buildSerializableObject(SerializableInterface $criteria): array
     {
         return  $this->executeOverrides(
