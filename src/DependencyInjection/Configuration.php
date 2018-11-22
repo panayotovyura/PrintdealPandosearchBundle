421e44eacd728e6f23802d3dff97b088add9b320
oromanchenko <o.romanchenko@levi9.com>
2018-11-22 11:05:19 +0200

    remove not needed annotation from configuration file

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -5,7 +5,6 @@ namespace Printdeal\PandosearchBundle\DependencyInjection;
 use Printdeal\PandosearchBundle\Entity\Search\DefaultResponse as SearchResponse;
 use Printdeal\PandosearchBundle\Entity\Suggestion\DefaultResponse as SuggestionResponse;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
-use Symfony\Component\Config\Definition\Builder\NodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -17,7 +16,6 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        /** @var ArrayNodeDefinition $rootNode */
         $rootNode = $treeBuilder->root('printdeal_pandosearch');
 
         $rootNode
